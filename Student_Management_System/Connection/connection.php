348e4adf820ad196148395b20f77c598177834f2
Prajwal Nirmal <111284294+PrajwalNirmal7@users.noreply.github.com>
2025-02-03 19:27:24 +0530

    Student Management System

---
+++ Student_Management_System/Connection/connection.php
@@ -0,0 +1,8 @@
+<?php 
+	$con=mysqli_connect("localhost","root","","imperial_college");
+	if(!$con)
+	{
+		echo "Connection is not Successfully";
+	}
+?>
+https://drive.google.com/drive/folders/1iWiVTdeSPAgZ09qfeJ0TUcZYfwP1rFYs
